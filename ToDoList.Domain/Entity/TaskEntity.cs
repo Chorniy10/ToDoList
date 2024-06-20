49dbb803af3c3e8618ea854c0b68bc2e5d6fa260
Chorniy10 <113625111+Chorniy10@users.noreply.github.com>
2024-06-20 03:40:22 +0300

    Add files via upload

--- ToDoList.Domain/Entity/TaskEntity.cs
+++ ToDoList.Domain/Entity/TaskEntity.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ToDoList.Domain.Enum;
+﻿using ToDoList.Domain.Enum;
 
-namespace ToDoList.Domain.Entity
+namespace ToDoList.Domain.Entity;
+
+public class TaskEntity
 {
-    public class TaskEntity
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsDone { get; set; }
-        public string Description { get; set; }
-        public DateTime Created { get; set; }
-        public Priority Priority { get; set; }
-    }
+    public long Id { get; set; }
+    
+    public string Name { get; set; }
+    
+    public bool IsDone { get; set; }
+    
+    public string Description { get; set; }
+    
+    public DateTime Created { get; set; }
+    
+    public Priority Priority { get; set; }
 }
