cef8d39806da6fdc0450a17150aea9f71eb99b6b
Chorniy10 <113625111+Chorniy10@users.noreply.github.com>
2024-06-20 03:39:00 +0300

    Add files via upload

--- ToDoLIst.Service/Interfaces/ITaskService.cs
+++ ToDoLIst.Service/Interfaces/ITaskService.cs
@@ -1,23 +1,16 @@
-﻿using Microsoft.AspNetCore.Components.Web;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ToDoList.Domain.Entity;
+﻿using ToDoList.Domain.Entity;
 using ToDoList.Domain.Filters.Task;
 using ToDoList.Domain.Response;
 using ToDoList.Domain.ViewModels.Task;
 
-namespace ToDoLIst.Service.Interfaces
-{
-    public interface ITaskService
-    {
-        Task<IBaseResponse<IEnumerable<TaskViewModel>>> CalculateCompletedTasks();
-        Task<IBaseResponse<IEnumerable<TaskCompletedViewModel>>> GetCompletedTasks();
-        Task<IBaseResponse<TaskEntity>> Create(CreateTaskViewModel model);
-        Task<IBaseResponse<bool>> EndTask(long id);
-        Task<DataTableResult> GetTasks(TaskFilter filter);
+namespace ToDoList.Service.Interfaces;
 
-    }
+public interface ITaskService
+{
+    Task<IBaseResponse<TaskEntity>> Create(CreateTaskViewModel model);
+    Task<IBaseResponse<bool>> EndTask(long id);
+    Task<DataTableResult>GetTasks(TaskFilter filter);
+    Task<IBaseResponse<IEnumerable<TaskViewModel>>> GetCompletedTasks();
+
+    Task<IBaseResponse<IEnumerable<TaskViewModel>>> CalculateCompletedTasks();
 }
