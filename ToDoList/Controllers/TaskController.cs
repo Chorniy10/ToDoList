dd1e7127fa9d191f646025b58e2b2c14511df783
Chorniy10 <113625111+Chorniy10@users.noreply.github.com>
2024-06-20 03:35:24 +0300

    Add files via upload

--- ToDoList/Controllers/TaskController.cs
+++ ToDoList/Controllers/TaskController.cs
@@ -1,79 +1,77 @@
+Ôªøusing Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using ToDoList.Domain.Filters.Task;
 using ToDoList.Domain.Helpers;
 using ToDoList.Domain.ViewModels.Task;
-using ToDoList.Models;
-using ToDoLIst.Service.Interfaces;
+using ToDoList.Service.Interfaces;
 
-namespace ToDoList.Controllers
+namespace ToDoList.Controllers;
+
+public class TaskController : Controller
 {
-    public class TaskController : Controller
+    private readonly ITaskService _taskService;
+
+    public TaskController(ITaskService taskService)
     {
-        private readonly ITaskService _taskService;
+        _taskService = taskService;
+    }
 
-        public TaskController(ITaskService taskService)
-        {
-            _taskService = taskService;
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CalculateCompletedTasks()
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateTaskViewModel model)
+    {
+        var response = await _taskService.Create(model);
+        if (response.StatusCode == Domain.Enum.StatusCode.OK)
         {
-            var response = await _taskService.CalculateCompletedTasks();
-            if (response.StatusCode == Domain.Enum.StatusCode.OK) 
-            {
-                var csvService = new CsvBaseService<IEnumerable<TaskViewModel>>();
-                var uploadFile = csvService.UploadFile(response.Data);
-                return File(uploadFile, "text/csv", $"—Ú‡ÚËÒÚËÍ‡ Á‡ {DateTime.Now.ToLongDateString()}.csv");
-            }
-
-            return BadRequest(new {description = response.Description});
+            return Ok(new { description = response.Description });
         }
+        return BadRequest(new { description = response.Description });
+    }
 
-        public async Task<IActionResult> GetCompletedTasks()
+    [HttpPost]
+    public async Task<IActionResult> CalculateCompletedTasks()
+    {
+        var response = await _taskService.CalculateCompletedTasks();
+        if (response.StatusCode == Domain.Enum.StatusCode.OK)
         {
-            var result = await _taskService.GetCompletedTasks();
-            return Json(new {data = result.Data});
+            var csvService = new CsvBaseService<IEnumerable<TaskViewModel>>();
+            var uploadFile = csvService.UploadFile(response.Data);
+            return File(uploadFile, "text/csv", $"–°—Ç–∞—Ç–∏—Å—Ç–∏–∫–∞ –∑–∞ {DateTime.Now.ToLongDateString()}.csv");
         }
+        return BadRequest(new { description = response.Description });
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> Create(CreateTaskViewModel model)
-        {
-            var response = await _taskService.Create(model);
-            if(response.StatusCode == Domain.Enum.StatusCode.OK) 
-            {
-                return Ok(new {description =  response.Description});
-            }
-            return BadRequest(new {description = response.Description});
-        }
+    public async Task<IActionResult> GetCompletedTasks()
+    {
+        var result = await _taskService.GetCompletedTasks();
+        return Json(new { data = result.Data });
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> EndTask(long id)
+    public async Task<IActionResult> EndTask(long id)
+    {
+        var response = await _taskService.EndTask(id);
+        if (response.StatusCode == Domain.Enum.StatusCode.OK)
         {
-            var response = await _taskService.EndTask(id);
-            if (response.StatusCode == Domain.Enum.StatusCode.OK)
-            {
-                return Ok(new { description = response.Description });
-            }
-            return BadRequest(new { description = response.Description });
+            return Ok(new { description = response.Description });
         }
+        return BadRequest(new { description = response.Description });
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> TaskHandler(TaskFilter filter)
-        {
-            var start = Request.Form["start"].FirstOrDefault();
-            var length = Request.Form["length"].FirstOrDefault();
-            var pageSize = length != null ? Convert.ToInt32(length) : 0;
-            var skip = start != null ? Convert.ToInt32(start) : 0;
-            filter.Skip = skip;
-            filter.PageSize = pageSize;
-            var response = await _taskService.GetTasks(filter);
-            return Json(new {recordsFiltered = response.Total, recordsTotal = response.Total, data = response.Data });
-        }
+    public async Task<IActionResult> TaskHandler(TaskFilter filter)
+    {
+        var start = Request.Form["start"].FirstOrDefault();
+        var length = Request.Form["length"].FirstOrDefault();
+
+        var pageSize = length != null ? Convert.ToInt32(length) : 0;
+        var skip = length != null ? Convert.ToInt32(start) : 0;
+
+        filter.Skip = skip;
+        filter.PageSize = pageSize;
+        var response = await _taskService.GetTasks(filter);
+        return Json(new {data = response.Data });
     }
 }
